e194b90d4fcbd030fe4ec3639f56f7b3ebfb7096
anirudhchakraborty <preetiujjal@gmail.com>
2025-10-13 13:32:28 +0530

    Feat: Rectified checkPrime

--- Maths & Combinatronics/checkPrime.java
+++ Maths & Combinatronics/checkPrime.java
@@ -3,16 +3,33 @@ import java.util.*;
 public class checkPrime {
     public static Scanner sc = new Scanner(System.in);
     public static checkPrime callPrime = new checkPrime();
+
     public static void main(String[] args) {
         System.out.println("Give me a Number to Check");
         int a = sc.nextInt();
 
-    System.out.println(callPrime.primeChecker(a));}
+        System.out.println(callPrime.primeChecker(a));
+    }
 
-    public String primeChecker(int a){
-        if (a%2==0) {
-            return "Even";
+    public List<Integer> primeChecker(int a) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 2; i <= a; i++) {
+            boolean isPrime = true;
+            
+            int sqrt = (int) Math.sqrt(i);
+            
+            for (int j = 2; j <=sqrt; j++) {
+                
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                list.add(i);
+            }
         }
-        return "Odd";
+        return list;
     }
 }
