a93a5988b1a363581d4c61d0fb5951f202cb764d
anirudhchakraborty <preetiujjal@gmail.com>
2025-10-17 02:18:44 +0530

    Built a full-blown GCD engine using factorization — slow but bulletproof

--- Maths & Combinatronics/FindGCD.java
+++ Maths & Combinatronics/FindGCD.java
@@ -5,55 +5,90 @@ public class FindGCD {
     static FindGCD caller = new FindGCD();
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ArrayList<Integer> list = new ArrayList<>();
+        
         System.out.println("Give 1st Num");
         int num1 = sc.nextInt();
         System.out.println("Give 2nd Num");
         int num2 = sc.nextInt();
 
-        if (num1 < num2 && num2 % num1 == 0) {
+        if (num1 == 0 && num2 == 0) {
+            System.out.println("GCD is undefined for both numbers being zero.");
+        }
+        else if (num1 == num2) {
+            System.out.println(num1 + " Is the GCD");
+        }
+        else if(num1 == 0 ){
+            System.out.println(num2 + " Is the GCD");
+        }
+        else if (num2 == 0) {
+            System.out.println(num1 + " Is the GCD");
+        }
+        else if (num1 < num2 && num2 % num1 == 0) {
             System.out.println(num1 + " Is the GCD");
         }
         else if (num2 < num1 && num1 % num2 == 0) {
             System.out.println(num2 + " Is the GCD");
         }
-        else if (num1 == 5) {
-            System.out.println(caller.primeChecker(num1));
-            System.out.println(caller.primeChecker(num2));
+        else {
+        caller.factoriser(num1, num2);
+        //System.out.println(result);
         }
     }
 
-    void GCDFinder(int num1, int num2) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
 
-        int squrtNum1 = (int) Math.sqrt(num1);
+    void factoriser (int a, int b){
+        List<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
+        List<Integer> list3 = new ArrayList<Integer>();
+        int GCD = 1;
 
-    }
-
-    public List<Integer> primeChecker(int a) {
-        List<Integer> list = new ArrayList<Integer>();
-
-        // tels if it is prime
-        boolean isPrime = true;
-
-        // Sqrt of the number
-        int sqrt = (int) Math.sqrt(a);
-
-        for (int i = 2; i <= sqrt; i++) {
-            if (a % i == 0) {
-                isPrime = false;
-                break;
-            }
-
-            if (isPrime) {
-                list.add(i);
+        for (int i = 2; i <= a; i++) {
+            while (a % i ==0) {
+                list1.add(i);
+                a /= i;
             }
         }
-        return list;
-    }
-
-    void factoriser (int a){
+       
+    
+        for (int i = 2; i <= b; i++) {
+            while (b % i ==0) {
+                list2.add(i);
+                b /= i;
+            }
+        }
+       
+        System.out.println("The Factors of 1st list");
+        System.out.println(list1);
         
+        System.out.println("The Factors of 2nd list");
+        System.out.println(list2);
+
+        if(list1.size()<list2.size()) {
+            for(int i = 0; i<list1.size(); i++){
+                if (list2.contains(list1.get(i))) {
+                    list3.add(list1.get(i));
+                    list2.remove(list1.get(i));
+                }
+            }
+        }
+        else{
+            for(int i = 0; i<list2.size(); i++){
+                if (list1.contains(list2.get(i))) {
+                    list3.add(list2.get(i));
+                    list1.remove(list2.get(i));
+
+                }
+            }
+        }
+
+        System.out.println("The Common Factors are ");
+        System.out.println(list3);
+
+        for(int i = 0; i<list3.size();i++){
+            GCD *= list3.get(i);
+        }
+        
+        System.out.println("the GCD is " + GCD);
     }
 }
