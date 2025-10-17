2a254da5773e81ccb429fc45e898d6dd2389f153
anirudhchakraborty <preetiujjal@gmail.com>
2025-10-17 23:52:58 +0530

    Auto Commit

--- Maths & Combinatronics/FindGCD.java
+++ Maths & Combinatronics/FindGCD.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 public class FindGCD {
     static Scanner sc = new Scanner(System.in);
-    static FindGCD caller = new FindGCD();
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        
+
         System.out.println("Give 1st Num");
         int num1 = sc.nextInt();
         System.out.println("Give 2nd Num");
@@ -14,81 +13,71 @@ public class FindGCD {
 
         if (num1 == 0 && num2 == 0) {
             System.out.println("GCD is undefined for both numbers being zero.");
-        }
-        else if (num1 == num2) {
+        } else if (num1 == num2) {
             System.out.println(num1 + " Is the GCD");
-        }
-        else if(num1 == 0 ){
+        } else if (num1 == 0) {
             System.out.println(num2 + " Is the GCD");
-        }
-        else if (num2 == 0) {
+        } else if (num2 == 0) {
             System.out.println(num1 + " Is the GCD");
-        }
-        else if (num1 < num2 && num2 % num1 == 0) {
+        } else if (num1 < num2 && num2 % num1 == 0) {
             System.out.println(num1 + " Is the GCD");
-        }
-        else if (num2 < num1 && num1 % num2 == 0) {
+        } else if (num2 < num1 && num1 % num2 == 0) {
             System.out.println(num2 + " Is the GCD");
-        }
-        else {
-        caller.factoriser(num1, num2);
-        //System.out.println(result);
+        } else {
+            factoriser(num1, num2);
         }
     }
 
-
-    void factoriser (int a, int b){
-        List<Integer> list1 = new ArrayList<Integer>();
-        List<Integer> list2 = new ArrayList<Integer>();
-        List<Integer> list3 = new ArrayList<Integer>();
+    static void factoriser(int a, int b) {
+        List<Integer> factorsA = new ArrayList<Integer>();
+        List<Integer> factorsB = new ArrayList<Integer>();
+        List<Integer> commonFactors = new ArrayList<Integer>();
         int GCD = 1;
 
         for (int i = 2; i <= a; i++) {
-            while (a % i ==0) {
-                list1.add(i);
+            while (a % i == 0) {
+                factorsA.add(i);
                 a /= i;
             }
         }
-       
-    
+
         for (int i = 2; i <= b; i++) {
-            while (b % i ==0) {
-                list2.add(i);
+            while (b % i == 0) {
+                factorsB.add(i);
                 b /= i;
             }
         }
-       
-        System.out.println("The Factors of 1st list");
-        System.out.println(list1);
-        
-        System.out.println("The Factors of 2nd list");
-        System.out.println(list2);
 
-        if(list1.size()<list2.size()) {
-            for(int i = 0; i<list1.size(); i++){
-                if (list2.contains(list1.get(i))) {
-                    list3.add(list1.get(i));
-                    list2.remove(list1.get(i));
+        System.out.println("The Factors of 1st list");
+        System.out.println(factorsA);
+
+        System.out.println("The Factors of 2nd list");
+        System.out.println(factorsB);
+
+        if (factorsA.size() < factorsB.size()) {
+            for (int i = 0; i < factorsA.size(); i++) {
+                if (factorsB.contains(factorsA.get(i))) {
+                    commonFactors.add(factorsA.get(i));
+                    factorsB.remove(factorsA.get(i));
                 }
             }
-        }
-        else{
-            for(int i = 0; i<list2.size(); i++){
-                if (list1.contains(list2.get(i))) {
-                    list3.add(list2.get(i));
-                    list1.remove(list2.get(i));
+        } else {
+            for (int i = 0; i < factorsB.size(); i++) {
+                if (factorsA.contains(factorsB.get(i))) {
+                    commonFactors.add(factorsB.get(i));
+                    factorsA.remove(factorsB.get(i));
 
                 }
             }
         }
 
         System.out.println("The Common Factors are ");
-        System.out.println(list3);
+        System.out.println(commonFactors);
 
-        for(int i = 0; i<list3.size();i++){
-            GCD *= list3.get(i);
+        for (int i = 0; i < commonFactors.size(); i++) {
+            GCD *= commonFactors.get(i);
         }
-        
+
         System.out.println("the GCD is " + GCD);
     }
 }
