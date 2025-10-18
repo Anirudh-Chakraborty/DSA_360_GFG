0c88e17f934c0841610f5045ed4a0e3994a1ba10
anirudhchakraborty <preetiujjal@gmail.com>
2025-10-18 10:43:54 +0530

    Decimal to Binary Rectified for Only Positiv e Integers

--- Maths & Combinatronics/DecimalToBinary.java
+++ Maths & Combinatronics/DecimalToBinary.java
@@ -16,44 +16,13 @@ public class DecimalToBinary {
 
 		if (decimal == 0) {
 			sb.append(0);
-		} else if (decimal > 0) {
+		} else {
 			while (decimal >= 1) {
 				int a = decimal % 2;
 				sb.append(a);
 				decimal = decimal / 2;
 			}
-		} else if (decimal < 0) {
-			decimal = decimal * -1;
-
-			while (decimal >= 1) {
-				int a = decimal % 2;
-				sb.append(a);
-				decimal = decimal / 2;
-			}
-
-			for (int i = 0; i < sb.length(); i++) {
-				if (sb.charAt(i) == '1') {
-					sb.setCharAt(i, '0');
-				}
-				if (sb.charAt(i) == '0') {
-					sb.setCharAt(i, '1');
-				}
-
-			
-			}
-				int num = Integer.parseInt(sb.toString());
-				
-				int carry = 1;
-				while (carry != 0) {
-					int temp = num & carry;
-					num = num ^ carry;
-					carry = temp << 1;
-				}
-
-				sb.setLength(0);
-				sb.append(carry);
-		}
-
+		} 
 		return sb.reverse();
 	}
 }
